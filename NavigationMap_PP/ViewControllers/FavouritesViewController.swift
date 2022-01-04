90ca37ec235574783365054f66e6f5c01ea10ba9
MorozovDeveloper <76910221+MorozovDeveloper@users.noreply.github.com>
2022-01-04 18:23:20 +0300

    add view, delete containerVC, add prepare

--- NavigationMap_PP/ViewControllers/FavouritesViewController.swift
+++ NavigationMap_PP/ViewControllers/FavouritesViewController.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class FavouritesViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var saveRoutes: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
 
     }
     
 
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
 
+
+extension FavouritesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = saveRoutes
+        
+        return cell
+        
+    }
+     
 }
--- NavigationMap_PP/ViewControllers/MapViewController.swift
+++ NavigationMap_PP/ViewControllers/MapViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     @IBOutlet weak var conteinerView: UIView!
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var directionTextField: UITextField!
+    
+    @IBOutlet weak var testTF: UITextField!
     
     let locationManager = CLLocationManager()
     var directionsArray: [MKDirections] = []
@@ -21,10 +23,8 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     let regionInMetres = 1_500.00
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        conteinerView.isHidden = true
         mapView.delegate = self
         
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // точность отображения
@@ -34,28 +34,52 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         
     }
     
-    
-    // удаляем старый маршрут при построении нового
-    func resetMapView(withNew directions: MKDirections, mapKit: MKMapView) {
-        mapKit.removeOverlays(mapKit.overlays)
-        directionsArray.append(directions)
-        let _ = directionsArray.map { $0.cancel() }
-        directionsArray.removeAll()
+    @IBAction func goButton(_ sender: Any) {
+        setupPlacemark(textLocation: directionTextField.text!)
     }
     
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Save" {
+            guard let saveFavourites = segue.destination as? FavouritesViewController else {return}
+            saveFavourites.saveRoutes = directionTextField.text
+        }
+    }
     
-    @IBAction func searchMenuButton(_ sender: Any) {
-        conteinerView.isHidden = false
-        
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        let favouritesVC = unwindSegue.source as! FavouritesViewController
+        testTF.text = favouritesVC.saveRoutes
+    }
+    
+    
+    func setupPlacemark(textLocation: String?) {
+            guard let location = textLocation else {return}
+            
+            let geocoder = CLGeocoder()
+            
+            geocoder.geocodeAddressString(location) { [self] (placemarks, error) in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                
+                guard let placemarks = placemarks else {return}
+                let placemark = placemarks.first
+                
+                let annotation = MKPointAnnotation()
+                annotation.title = "От: \(textLocation!)"
+                guard let placeMarkLocation = placemark?.location else {return}
+                annotation.coordinate = placeMarkLocation.coordinate
+                
+                mapView?.showAnnotations([annotation], animated: true)
+                mapView?.selectAnnotation(annotation, animated: true)
+                
+                mapThis(destinationCord: placeMarkLocation.coordinate)
+        }
     }
     
     @IBAction func myLocationButton(_ sender: Any) {
         userLocation(mapKit: mapView)
-    }
-    
-    @IBAction func favouritesButton(_ sender: Any) {
-
     }
     
     // Нахождение юзера
@@ -70,26 +94,23 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
-    }
     
     func mapThis(destinationCord: CLLocationCoordinate2D) {
-        
+
         let sourceCordinate = (locationManager.location?.coordinate)!
-        
+
         let sourcePlaceMark = MKPlacemark(coordinate: sourceCordinate)
-        let destPlaceMArk = MKPlacemark(coordinate: destinationCord)
-        
+        let destPlaceMark = MKPlacemark(coordinate: destinationCord)
+
         let sourceItem = MKMapItem(placemark: sourcePlaceMark)
-        let destItem = MKMapItem(placemark: destPlaceMArk)
-        
+        let destItem = MKMapItem(placemark: destPlaceMark)
+
         let destinationRequest = MKDirections.Request()
         destinationRequest.source = sourceItem
         destinationRequest.destination = destItem
         destinationRequest.transportType = .automobile
         destinationRequest.requestsAlternateRoutes = true
-        
+
         let directions = MKDirections(request: destinationRequest)
         directions.calculate { (response, error) in
             guard let response = response else {
@@ -98,12 +119,21 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                 }
                 return
             }
-            
+
+            self.resetMapView(withNew: directions, mapKit: self.mapView) // удаляем старый маршрут при построении нового
+
             let route = response.routes[0]
             self.mapView.addOverlay(route.polyline)
             self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
-            
         }
+    }
+    
+    // удаляем старый маршрут при построении нового
+    func resetMapView(withNew directions: MKDirections, mapKit: MKMapView) {
+        mapKit.removeOverlays(mapKit.overlays)
+        directionsArray.append(directions)
+        let _ = directionsArray.map { $0.cancel() }
+        directionsArray.removeAll()
     }
     
     // Линия
@@ -117,25 +147,12 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     
     
-    
-    
-    
-    
-    // Пока не нужно
-    // Определяем центр карты
-//    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
-//        let latitude = mapView.centerCoordinate.latitude
-//        let longitude = mapView.centerCoordinate.longitude
-//
-//        return CLLocation(latitude: latitude, longitude: longitude)
-//    }
 //
 //    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-//        let center = getCenterLocation(for: mapView)
+//        let center = CLLocation(latitude: mapView.centerCoordinate.latitude , longitude: mapView.centerCoordinate.longitude) //центр карты
 //        let geocoder = CLGeocoder()
 //
 //        geocoder.reverseGeocodeLocation(center) { (placemarks, error) in
-//
 //            if let error = error {
 //                print(error)
 //                return
@@ -146,15 +163,17 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 //            let streetName = placemark?.thoroughfare // определяем название улицы
 //            let buildNumber = placemark?.subThoroughfare // определяем номер дома
 //
+//
 //            DispatchQueue.main.async {
 //                if streetName != nil && buildNumber != nil {
-//                    self.testLabel.text = "\(streetName!), \(buildNumber!)"
-//                } else if streetName != nil {
-//                    self.testLabel.text = "\(streetName!)"
-//                } else {
-//                    self.testLabel.text = ""
-//                }
+//                        self.testLabel.text = "\(streetName!), \(buildNumber!)"
+//                    } else if streetName != nil {
+//                        self.testLabel.text = "\(streetName!)"
+//                    } else {
+//                        self.testLabel.text = ""
+//
 //            }
+//          }
 //        }
 //    }
     
